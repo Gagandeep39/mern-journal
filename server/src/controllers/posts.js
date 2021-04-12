d4d66166119c5d965d01e1cbffac8f58c7db726d
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-12 09:55:44 +0000

    Added create and fetch Logic

--- server/src/controllers/posts.js
+++ server/src/controllers/posts.js
@@ -1,3 +1,23 @@
-export const getPosts = (req, res) => {
-  res.send('Post GET Route');
+import Post from '../models/post.js';
+
+export const getPosts = async (req, res) => {
+  try {
+    const postMessages = await Post.find();
+    res.status(200).json(postMessages);
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  try {
+    const post = req.body;
+    const newPost = new Post(post);
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    console.log(error.message);
+    res.status(409).json({ message: error.message });
+  }
 };

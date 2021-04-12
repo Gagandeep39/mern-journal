d441da536408d65c517c1118604c65c9fb355e40
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-12 10:16:15 +0000

    Created  basic structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
+import { AppBar, Container, Grid, Grow, Typography } from '@material-ui/core';
+import Journal from './assets/journal.svg';
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container maxWidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center"></Typography>
+        <img src={Journal} alt="Journal" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 }
 
 export default App;

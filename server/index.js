9553167eca4f6c1b9b88027be73c49e74488dd37
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-12 08:12:44 +0000

    Setup node server with mongo connection

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGO_URI?.toString(), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`Server Running at Port: ${process.env.PORT}`)
+    )
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
